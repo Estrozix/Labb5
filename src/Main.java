869c19a7f04ff576567511de8024c9cf100d7335
Estrozix <simonnilsson1997@gmail.com>
2017-02-08 13:18:45 +0100

    Adding javadoc to Main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The main class for the program. It basically starts the main thread for Swing and creates the window.
+ */
 public class Main extends JFrame {
 
     private CrystalControl crystalControl;
 
     private int size = 600;
 
+    /**
+     * Creates the JFrame and sets some properties, also creates the crystalControl object.
+     */
     public Main() {
         this.setTitle("Crystal");
         this.setVisible(true);
